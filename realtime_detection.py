2703e6f62a8a25242cb4180a6852f62adc512a84
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2022-05-22 00:37:08 +0530

    Update realtime_detection.py

--- realtime_detection.py
+++ realtime_detection.py
@@ -208,3 +208,41 @@ def bbox_to_bytes(bbox_array):
   bbox_bytes = 'data:image/png;base64,{}'.format((str(b64encode(iobuf.getvalue()), 'utf-8')))
 
   return bbox_bytes
+
+
+
+# start streaming video from webcam
+video_stream()
+# label for video
+label_html = 'Capturing...'
+# initialze bounding box to empty
+bbox = ''
+count = 0 
+while True:
+    js_reply = video_frame(label_html, bbox)
+    if not js_reply:
+        break
+
+    # convert JS response to OpenCV Image
+    frame = js_to_image(js_reply["img"])
+
+    # create transparent overlay for bounding box
+    bbox_array = np.zeros([480,640,4], dtype=np.uint8)
+
+    # call our darknet helper on video frame
+    detections, width_ratio, height_ratio = darknet_helper(frame, width, height)
+
+    # loop through detections and draw them on transparent overlay image
+    for label, confidence, bbox in detections:
+      left, top, right, bottom = bbox2points(bbox)
+      left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
+      bbox_array = cv2.rectangle(bbox_array, (left, top), (right, bottom), class_colors[label], 2)
+      bbox_array = cv2.putText(bbox_array, "{} [{:.2f}]".format(label, float(confidence)),
+                        (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        class_colors[label], 2)
+
+    bbox_array[:,:,3] = (bbox_array.max(axis = 2) > 0 ).astype(int) * 255
+    # convert overlay of bbox into bytes
+    bbox_bytes = bbox_to_bytes(bbox_array)
+    # update bbox so next frame gets new overlay
+    bbox = bbox_bytes
