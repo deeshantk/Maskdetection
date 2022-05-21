c61ea72da7516f81680bc26537240be198518350
Deeshant Kotnala <44977226+deeshantk@users.noreply.github.com>
2022-05-22 00:37:51 +0530

    Create detection_on_video.py

---
+++ detection_on_video.py
@@ -0,0 +1,5 @@
+!./darknet detector demo /content/drive/MyDrive/Face_Mask_Detection_YOLO/MASK/object.data\
+                         /content/drive/MyDrive/Face_Mask_Detection_YOLO/MASK/detect_mask.cfg\
+                         /content/drive/MyDrive/yolov3_mask_last.weights\
+                         -dont_show\
+                         /content/Face_Mask_Detection_YOLO/demo/190312_32_StreetLife_HD_009_cropped.mp4 -i 0 -out_filename results.avi
